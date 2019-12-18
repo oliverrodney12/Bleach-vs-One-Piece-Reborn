fb7c9923abaa53518dff14a650a3f42417d0f5a4
oliverrodney12 <57301416+oliverrodney12@users.noreply.github.com>
2019-12-19 02:36:44 +0800

    Update bvo_special_bonus_lifesteal_5_modifier.lua

--- game/x_arena/scripts/vscripts/talents/bvo_special_bonus_lifesteal_5_modifier.lua
+++ game/x_arena/scripts/vscripts/talents/bvo_special_bonus_lifesteal_5_modifier.lua
@@ -1,10 +1,10 @@
 bvo_special_bonus_lifesteal_5_modifier = class ({})
 
-function bvo_special_bonus_lifesteal_10_modifier:IsHidden()
+function bvo_special_bonus_lifesteal_5__modifier:IsHidden()
 	return true
 end
 
-function bvo_special_bonus_lifesteal_10_modifier:DeclareFunctions()
+function bvo_special_bonus_lifesteal_5_modifier:DeclareFunctions()
 	local funcs = {
 		MODIFIER_EVENT_ON_ATTACK_LANDED,
 	}
@@ -12,10 +12,10 @@ function bvo_special_bonus_lifesteal_10_modifier:DeclareFunctions()
 	return funcs
 end
 
-function bvo_special_bonus_lifesteal_10_modifier:OnAttackLanded( params )
+function bvo_special_bonus_lifesteal_5_modifier_modifier:OnAttackLanded( params )
 	if IsServer() then
 		local attacker = params.attacker
-		if attacker:HasModifier("bvo_special_bonus_lifesteal_10_modifier") then
+		if attacker:HasModifier("bvo_special_bonus_lifesteal_5_modifier") then
 			local healGained = params.damage * 0.05
 			attacker:Heal( healGained, nil)
 			
